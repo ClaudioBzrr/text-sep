aaa2604d8de92cdc165e84fe28cdc27c944f7feb
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2022-10-05 14:38:53 +0000

    regexp added on function

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,15 +1,58 @@
+import { useState } from "react"
 
 
 export function Home(){
+
+    const [separator,setSeparator] =  useState<string>(",")
+    const [text,setText] =  useState<string>("")
+    const [range,setRange] =  useState<number>(10)
+    const [result,setResult] =  useState<string>("")
+
+
+    function applySeparatorRules():string{
+        const applyRange =  new RegExp(`.{1,${range}}`,'g')
+        const parts:RegExpMatchArray| null = text.match(applyRange)
+        if(parts){
+            const result:string = parts.join(`${separator}`)
+
+            return result
+        }else{
+            throw new Error("Tamanho do texto menor que o intervalo especificado")
+        }
+    }
+
+    function handleInputChange(words:string){
+        if(words.length >0){
+
+            setText(words)
+            try{
+                setResult(applySeparatorRules())
+            }catch(err){
+                alert(err)
+            }
+        }else{
+            setResult("")
+        }
+    }
+
+
     return(
-        <div className="w-screen h-screen flex flex-row items-center justify-around">
+        <div className="w-screen h-screen flex flex-row items-center justify-around bg-[#191622]">
 
             <div>
-                <input type="text" placeholder="Coloque seu texto aqui" className="input input-bordered w-[30vw] h-[40vh]"/>
+                <input
+                    onChange={e => handleInputChange(e.currentTarget.value)} 
+                    type="text" 
+                    placeholder="Coloque seu texto aqui" 
+                    className="text-center bg-[#41414D] rounded-lg w-[30vw] h-[40vh] font-semibold text-lg text-[#fff]"
+                />
+
             </div>
 
-            <div className="w-[30vw] h-[40vh]">
-
+            <div className="text-center bg-[#41414D] rounded-lg w-[30vw] h-[40vh]">
+                {
+                    result
+                }
             </div>
 
         </div>
