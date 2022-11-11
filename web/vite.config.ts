256b3513ec8efaba28cbd9ee7dce1f4d62bd4fe9
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2022-11-11 11:48:09 +0000

    gh-pages deploy added

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/text-sep/',
+  build:{
+    outDir:'../web/build'
+  },
   plugins: [react()]
 })
